22574bc18dd060ad21a6773c2142e4066c9f60c1
Tommi <thomas.innerlohinger@gmail.com>
2019-02-14 15:00:34 +0100

    customer detail

--- customer_detail.php
+++ customer_detail.php
@@ -1,17 +1,85 @@
 <?php include('connection.php') ?>
 <?php include('header.php') ?>
-<?php $kundennummer = $_GET['kundennummer']; ?>
 
 <!-- <body> from header.php -->
 
-<?php            
-$query = "SELECT * from kunde WHERE kundennummer = '{$kundennummer};
+<?php
+$kundennummer = $_GET['kundennummer'];
+
+$sql = "SELECT * FROM kunde WHERE kundennummer = '$kundennummer'";
+
+$stmt = $pdo->prepare("SELECT * FROM kunde WHERE kundennummer = '$kundennummer'");
+$stmt->execute();
+$result = $stmt->fetch();
+
+
+
 ?>
 
     <table class="table table-striped table-hover ml-2 mr-2">
             <tr>
                 <td>Kd.Nr.</td>
-                <td><?php $query['kundennummer']?></td>
+                <td><?php echo $result['kundennummer'] ?></td>
+            </tr>
+            <tr>
+                <td>Anrede</td>
+                <td><?php echo $result['anrede'] ?></td>
+            </tr>
+            <tr>
+                <td>Titel</td>
+                <td><?php echo $result['titel'] ?></td>
+            </tr>
+            <tr>
+                <td>Vorname</td>
+                <td><?php echo $result['vorname'] ?></td>
+            </tr>
+            <tr>
+                <td>Nachname</td>
+                <td><?php echo $result['nachname'] ?></td>
+            </tr>
+            <tr>
+                <td>Geburtsdatum</td>
+                <td><?php echo $result['gebdat'] ?></td>
+            </tr>
+            <tr>
+                <td>Strasse</td>
+                <td><?php echo $result['strasse'] ?></td>
+            </tr>
+            <tr>
+                <td>PLZ</td>
+                <td><?php echo $result['plz'] ?></td>
+            </tr>
+            <tr>
+                <td>Ort</td>
+                <td><?php echo $result['ort'] ?></td>
+            </tr>
+            <tr>
+                <td>Telefon</td>
+                <td><?php echo $result['telefon'] ?></td>
+            </tr>
+            <tr>
+                <td>EMail</td>
+                <td><?php echo $result['email'] ?></td>
+            </tr>
+            <tr>
+                <td>Newsletter</td>
+                <td>
+                    <?php 
+                        if ($result['newsletter'] == 1) {
+                            echo "Ja";
+                        } else {
+                            echo "Nein";
+                        } 
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Kommentar</td>
+                <td><?php echo $result['kommentar'] ?></td>
+            </tr>
+            <tr>
+                <td>Kunde seit</td>
+                <td><?php echo $result['kundeseit'] ?></td>
             </tr>
         
     </table>
