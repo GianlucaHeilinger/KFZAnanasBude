6d7a54f1040bd1dee3c8a78daa64ea5abc93117d
Gianluca <gianlucaheilinger@gmail.com>
2019-02-14 17:15:36 +0100

    auto Datum

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
-function giveActualDate() {           
-    document.getElementById('gebdat').valueAsDate = new Date();
+function giveActualDate() {      
+    try{
+        document.getElementById('gebdat').valueAsDate = new Date();
+    }catch(err1){}
+
+    try{
     document.getElementById('kundeseit').valueAsDate = new Date();
+    }catch(err2){}
+
+    try{
     document.getElementById('ReparaturDatum').valueAsDate = new Date();
+    }catch(err3){}
 }
