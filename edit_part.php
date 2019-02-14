dfa7143f5d7f2c7e726dd666358b5b03eaa90795
Gianluca <gianlucaheilinger@gmail.com>
2019-02-14 15:49:17 +0100

    teile edit funktioniert

--- edit_part.php
+++ edit_part.php
@@ -8,14 +8,15 @@ $id=$_GET["teileid"];
 
 $sql ="SELECT * FROM teile WHERE teileid = $id";
 
+echo "<form form action='' method='post'>";
 echo "<table class='table table-striped table-hover ml-2 mr-2>'";
 
 foreach ($pdo->query($sql) as $row) {
     echo "<tr><td>Teile Nr.</td>";
-    echo "<td>" . $row['teileid'] . "</td>"; 
+    echo "<td><p name='txtid'" . $row['teileid'] . "/></td>"; 
 
     echo "<tr><td>Bezeichnung</td>";
-    echo "<td><input type='text' value='" . $row['bezeichnung'] . "'/></td>"; 
+    echo "<td><input name='txtBezeichnung' type='text' value='" . $row['bezeichnung'] . "'/></td>"; 
 
     echo "<tr><td>Art</td>";
     if($row['teileart']=="Lohn")
@@ -29,12 +30,30 @@ foreach ($pdo->query($sql) as $row) {
     
 
     echo "<tr><td>Preis</td>";
-    echo "<td><input maxlength='11' name='txtPreis' Id='preis' value=".$row['preis']." type='number' step='0.01' /></td>";
+    echo "<td><input name='txtPreis' maxlength='11' name='txtPreis' Id='preis' value=".$row['preis']." type='number' step='0.01' /></td>";
+
+    break;
 } 
 
 echo "<tr><td><label for='submit'></label></td><td><input type='submit' name='Speichern' value='Speichern' class='btn btn-dark'></td></tr>";
 
 echo "</table>";
+echo "</form>";
+
+if(isset($_POST['Speichern']))
+{
+    $Bezeichnung=$_POST["txtBezeichnung"];
+    $Preis=$_POST["txtPreis"];
+    $Art=$_POST["teileart"];
+
+   // $sql = "UPDATE teile SET bezeichnung = ".$Bezeichnung.",teileart=".$Art.", preis=".$Preis." WHERE teile.teileid =". $id;
+
+    $sql = "UPDATE teile SET bezeichnung=?, teileart=?, preis=? WHERE teileid=?";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute([$Bezeichnung, $Art, $Preis,$id]);
+
+    header("Refresh: 0; url=part_list.php");
+}
 ?>
 
 <?php include('footer.php') ?>
