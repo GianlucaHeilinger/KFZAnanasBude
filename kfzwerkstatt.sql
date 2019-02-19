729f737c88851e5c247cb4da0c4fa7683a86c682
Thomas Innerlohinger <ti@x-net.at>
2019-02-19 11:06:39 +0100

    customer changes

--- kfzwerkstatt.sql
+++ kfzwerkstatt.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 14. Feb 2019 um 13:39
--- Server-Version: 10.1.34-MariaDB
--- PHP-Version: 7.2.8
+-- Host: localhost
+-- Erstellungszeit: 19. Feb 2019 um 10:58
+-- Server-Version: 5.7.25-0ubuntu0.18.04.2
+-- PHP-Version: 7.2.15-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -45,6 +43,27 @@ CREATE TABLE `fahrzeug` (
   `erstzulassung` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 
+--
+-- Daten für Tabelle `fahrzeug`
+--
+
+INSERT INTO `fahrzeug` (`fzid`, `kundeid`, `marke`, `type`, `kennzeichen`, `fahrgestellnummer`, `nationalcode`, `motorkennzeichen`, `getriebekennzeichen`, `farbe`, `treibstoff`, `leistung`, `hubraum`, `erstzulassung`) VALUES
+(5, 11, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(6, 11, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(7, 11, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(8, 11, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(9, 11, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(10, 11, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(11, 12, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(12, 12, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(13, 12, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(14, 12, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(15, 12, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(16, 12, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(17, 13, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(18, 13, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29'),
+(19, 13, 'Audi', 'A4', 'S-1234-AB', '123', '123', '123', '123', 'schwarz', 'Benzin', 120, 1800, '2017-10-29');
+
 -- --------------------------------------------------------
 
 --
@@ -63,10 +82,32 @@ CREATE TABLE `kunde` (
   `ort` varchar(50) COLLATE utf8_german2_ci NOT NULL,
   `telefon` varchar(20) COLLATE utf8_german2_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_german2_ci NOT NULL,
-  `newsletter` tinyint(1) NOT NULL,
+  `newsletter` tinyint(1) DEFAULT NULL,
   `kommentar` text COLLATE utf8_german2_ci NOT NULL,
   `kundeseit` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+
+--
+-- Daten für Tabelle `kunde`
+--
+
+INSERT INTO `kunde` (`kundennummer`, `anrede`, `titel`, `vorname`, `nachname`, `gebdat`, `strasse`, `plz`, `ort`, `telefon`, `email`, `newsletter`, `kommentar`, `kundeseit`) VALUES
+(11, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(12, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(13, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(14, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(15, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(16, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(17, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(18, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(19, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(20, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(21, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(22, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(23, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(24, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(25, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19'),
+(26, 'Herr', 'Dr.', 'Max', 'Mustermann', '1977-12-16', 'Musterstrasse 5', 1234, 'Musterhausen', '067612345678', 'max@mustermann.at', 1, 'Toller Kunde!', '2018-02-19');
 
 -- --------------------------------------------------------
 
@@ -207,38 +248,32 @@ ALTER TABLE `teile`
 -- AUTO_INCREMENT für Tabelle `fahrzeug`
 --
 ALTER TABLE `fahrzeug`
-  MODIFY `fzid` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `fzid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT für Tabelle `kunde`
 --
 ALTER TABLE `kunde`
-  MODIFY `kundennummer` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+  MODIFY `kundennummer` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT für Tabelle `rechnungdetails`
 --
 ALTER TABLE `rechnungdetails`
   MODIFY `rechnungdetailid` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT für Tabelle `reparatur`
 --
 ALTER TABLE `reparatur`
   MODIFY `repid` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT für Tabelle `reparaturdetails`
 --
 ALTER TABLE `reparaturdetails`
   MODIFY `repdetid` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT für Tabelle `teile`
 --
 ALTER TABLE `teile`
   MODIFY `teileid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
 --
 -- Constraints der exportierten Tabellen
 --
@@ -275,7 +310,6 @@ ALTER TABLE `reparatur`
 ALTER TABLE `reparaturdetails`
   ADD CONSTRAINT `reparaturdetails_ibfk_1` FOREIGN KEY (`repid`) REFERENCES `reparatur` (`repid`),
   ADD CONSTRAINT `reparaturdetails_ibfk_2` FOREIGN KEY (`teileid`) REFERENCES `teile` (`teileid`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
